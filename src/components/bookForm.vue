4be5e61fbc91e28aef9f3308097814eb29577b39
mateusz2247 <mateusz2247@gmail.com>
2023-04-06 09:45:38 +0200

    zadanie z rozdzialu 1.4 poprawione czyszczenie

--- src/components/bookForm.vue
+++ src/components/bookForm.vue
@@ -1,36 +1,42 @@
 <template>
-	<form @submit.prevent="submit(newBookTittle,newBookPrice)" >
+	<form @submit.prevent="submit(newBookTittle, newBookPrice)">
 		<label>
 			Title:
 			<input v-model="newBookTittle" type="text" name="title" />
-       
 		</label>
 		<label>
 			price:
 			<input v-model="newBookPrice" type="number" name="price" />
 		</label>
-		<button>Add book </button>
+		<button>Add book</button>
 	</form>
+
+	<!-- <div>
+		<h5>Products</h5>
+		<ul>
+			<li v-for="(product,index) in products" :key="index">{{ product.book.title }}</li>
+		</ul>
+	</div> -->
 </template>
 
 <script setup>
-
 import { ref } from "vue";
-import {defineEmits} from 'vue';
-const emits = defineEmits(['submit'])
+import { defineEmits } from "vue";
+const emits = defineEmits(["submit"]);
 
 let newBookTittle = ref();
 let newBookPrice = ref(0);
 
-   
-const submit=(newBookTittle,newBookPrice)=>{
-    emits("submit",newBookTittle,newBookPrice);
-    clear()
-}
+const submit = (newBookTittle2, newBookPrice2) => {
+	emits("submit", newBookTittle2, newBookPrice2);
+	newBookTittle.value = "";
+	newBookPrice.value = "";
+};
 
-function clear(){
-    newBookTittle= ''
-    newBookPrice=''
-}   
+/* const products = ref(null);
+fetch("https://api.itbook.store/1.0/new")
+	.then((response) => response.json())
+	.then((data) => (products.value = data));
 
+console.log(products); */
 </script>
--- src/components/booksList.vue
+++ src/components/booksList.vue
@@ -8,9 +8,12 @@
 		</ul>
     </div>
 
+
+
 </template>
 
 <script setup>
+
     import { defineProps } from "vue";
     const props = defineProps({
         books: Array
